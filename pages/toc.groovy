02a96b8bf89551d20e396ad761db8b822fa785c5
neodevelop <neodevelop@gmail.com>
2013-12-31 11:57:21 -0600

    creando el temario

--- pages/toc.groovy
+++ pages/toc.groovy
@@ -5,54 +5,57 @@
   "presentacion.html#4"(title: "Herramientas para entrenamiento")
 }
 
-"essential.html"(title: "Elementos esenciales de Springframework"){
-  "essential.html"(title: "Módulos en Spring")
-  "essential.html"(title: "Diseño de aplicaciones(orientado a interfaces)")
-}
+"essentials.html"(title: "Elementos esenciales del MVC")
 
-"ioc.html"(title: "Inversion of control y Dependency Injection"){
-
-}
-
-"spring_container.html"(title: "El conteneder de Spring"){
-  "spring_container.html"(title: "Declaración de beans")
-  "spring_container.html"(title: "Inyección por constructor")
-  "spring_container.html"(title: "Inyecciones por setter")
-  "spring_container.html"(title: "Wiring")
-  "spring_container.html"(title: "Ciclo de vida de los beans")
-  "spring_container.html"(title: "AppCtx y BeanFactory")
-}
-
-"appctx.html"(title:"Configuración del AppCtx"){
-  "appctx.html"(title:"Configuración con XML"){
-    "appctx.html"(title:"Namespaces")  
+"architecture.html"(title: "Arquitectura de las aplicaciones Spring MVC"){
+  "architecture.html"(title: "Ciclo de vida del request")
+  "architecture.html"(title: "El DispatcherServlet")
+  "architecture.html"(title: "Elementos esenciales de SpringMVC"){
+    "architecture.html"(title: "HandlerMapping")
+    "architecture.html"(title: "ViewResolver")
+    "architecture.html"(title: "Handler Interceptors")
   }
-  "appctx.html"(title:"Configuración con Anotaciones")
-  "appctx.html"(title:"Configuración con JavaConfig")
-  "appctx.html"(title:"Configuración con Groovy")
-
 }
 
-"spel.html"(title:"Spring Expression Language")
-
-"aop.html"(title:"Programación orientada a aspectos"){
-  "aop.html"(title:"Conceptos esenciales"){
-    "aop.html"(title:"JoinPoint")
-    "aop.html"(title:"Pointcut")
-    "aop.html"(title:"Expresiones")  
-  }
-  "aop.html"(title:"Declarando aspectos con Anotaciones"){
-    "aop.html"(title:"Soporte de anotaciones con AspectJ")
-    "aop.html"(title:"Declaración de aspectos(Advice)")
-    "aop.html"(title:"Before")
-    "aop.html"(title:"After")
-    "aop.html"(title:"After returning")
-    "aop.html"(title:"After throwing")
-    "aop.html"(title:"Around ")  
-  }
-  
-  "aop.html"(title:"JoinPoint en detalle")
-  "aop.html"(title:"Declarando aspectos con XML")
+"configuration.html"(title: "Configuración esencial de SpringMVC"){
+  "configuration.html"(title: "Uso del namespace MVC de SpringMVC")
+  "configuration.html"(title: "Uso del MVC Java Config")
 }
+
+"views_controllers.html"(title: "Vistas y controllers"){
+  "views_controllers.html"(title: "Declaración de controllers")
+  "views_controllers.html"(title: "Uso de @RequestMapping y URI Templates"){
+    "views_controllers.html"(title: "Captura de parámetros - @RequestParam")
+    "views_controllers.html"(title: "Modelos y atributos - @ModelAttribute")
+  }
+}
+
+"forms.html"(title:"Manejo de formularios"){
+  "forms.html"(title:"Implementación de FormController")
+  "forms.html"(title:"Tags de Spring MVC")
+  "forms.html"(title:"Validaciones")
+  "forms.html"(title: "Elementos en sesión - @SessionAttributes")
+  "forms.html"(title:"Uso de @InitBinder")
+  "forms.html"(title:"Upload de archivos(MultipartResolver)")
+}
+
+"customize.html"(title:"Internacionalización y temas"){
+  "customize.html"(title:"Internacionalización: LocaleResolver y LocaleChangeInterceptor")
+  "customize.html"(title:"Decoración: ThemeResolver y ThemeChangeInterceptor")
+  "customize.html"(title:"Diseño visual transversal")
+}
+
+"exceptions.html"(title:"Manejo de errores en la aplicación"){
+  "exceptions.html"(title:"Por Contrroller(@ExceptionResolver)")
+  "exceptions.html"(title:"Uso del HandlerExceptionResolver")
+}
+
+"views.html"(title:"Generación de otras vistas"){
+  "views.html"(title:"Documentos en PDF")
+  "views.html"(title:"Integración con JasperReports")
+  "views.html"(title:"Documentos de Excel")
+}
+
+"boot.html"(title:"Spring Boot")
 
 "agradecimiento.html"(title: "Agradecimiento")
